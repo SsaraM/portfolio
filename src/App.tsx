cdc0611fa4c5506c958c3f961eb440e7aef6fef6
SsaraM <sara.mustapha26@gmail.com>
2022-11-01 15:57:03 +0200

    valmis

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 // App.tsx
 import React from 'react'
 import MyFirstComponent from './components/myFirstComponent';
+import SomeExampleComponent from './components/example/someExampleComponent';
+import ClickCount from './components/clickCount';
 
 const App: React.FC = () => {
   return (
     <div>
+      asd
       <MyFirstComponent />
+      <SomeExampleComponent />
+      <ClickCount />
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+// import styles from './index.module.scss'
+import SomeExampleComponent from './components/example/someExampleComponent';
+import MyFirstComponent from './components/myFirstComponent';
+import Navbar from './components/navbar/navbar';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='example' element={<SomeExampleComponent />} />
+        <Route path='/first' element={<MyFirstComponent />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
+
