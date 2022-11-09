d9907a30fe34dee2c0f7971ab45a31198228ea40
SsaraM <sara.mustapha26@gmail.com>
2022-11-09 13:47:20 +0200

    responsiiviset sivut

--- src/components/portfolioSara/cvsivu.tsx
+++ src/components/portfolioSara/cvsivu.tsx
@@ -8,13 +8,15 @@ type CvsivuProps = {
 const Cvsivu: React.FC<CvsivuProps> = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.left}>
-      <h1>Koulutus</h1>
-      <h2>Stadin ammattiopisto Tekstiili- ja vaatetusalan perustutkinto 2017</h2>
-        </div>
-        <div className={styles.right}>
-      <h1>Työkokemus</h1>
-        </div>
+      <div className={styles.left}>
+        <h1>Koulutus</h1>
+        <h2>Stadin ammattiopisto Tekstiili- ja vaatetusalan perustutkinto 2017</h2>
+        <h2>Vantaan ammattiopisto Ohjelmistokehittäjä 2021 -</h2>
+      </div>
+      <div className={styles.right}>
+        <h1>Työkokemus</h1>
+        <h2>Työharjoittelu koululla tulee tähän</h2>
+      </div>
     </div>
   )
 }
